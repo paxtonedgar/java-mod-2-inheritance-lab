a494fb7607c1f7f52305e255f20c3805e96ea42f
Paxton Edgar <paxtonedgar3@gmail.com>
2022-06-29 15:05:55 -0400

    init push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Running my pet examples");
-        pet basepet = new pet();
-        basepet.needFood(); 
-        dog pooch = new dog();
-        pooch.chewSomething();
-        pooch.play();
-        fish nemo = new fish(); 
-        nemo.exist();
+        pet basepet = new pet(); 
+        basepet = new dog();
+        System.out.println(basepet.getClass().toString());
+
     }
 }
